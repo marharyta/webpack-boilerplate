5424d06535b6ec49b86c3deab20235f79f63d1a4
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-06-23 18:47:22 +0300

    Add postcss support for CSS files

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
   },
