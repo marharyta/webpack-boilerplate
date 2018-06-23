3923594895fd6db0a983293a567e3e3492036b1b
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-06-23 20:58:13 +0300

    Update ESLint settings

--- webpack-development-setup/.eslintrc.js
+++ webpack-development-setup/.eslintrc.js
@@ -1,39 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended", "prettier",
-        "plugin:prettier/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier"
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'none', ignoreRestSiblings: false }
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "warn",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "prettier/prettier": "error"
-    }
+    'prettier/prettier': 'error'
+  }
 };
