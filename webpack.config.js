66da3b2fd6cae7f95efeefe1940fca63147fe04c
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-06-23 19:03:14 +0300

    Add clean-webpack-plugin to clear dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -26,6 +27,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {}),
     new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css',
     }),
