ebd0235b86558791f954b1b2595f20298b09bb9f
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-06-23 18:04:56 +0300

    Add first basic webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// webpack v4
+const path = require('path');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
